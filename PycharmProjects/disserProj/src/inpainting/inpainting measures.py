ca2970d804e33a00d1e5a1b1f85742d3cab58854
Udovichenko Danil <d.udovichenko@corp.mail.ru>
2019-05-11 12:55:42 +0300

    inpainting tests changes

--- PycharmProjects/disserProj/src/inpainting/inpainting measures.py
+++ PycharmProjects/disserProj/src/inpainting/inpainting measures.py
@@ -19,48 +19,50 @@ if __name__ == "__main__":
     outputPath2 = "../../src/inpainting/Telea.png"
     img = cv.imread(imagePath, 0)
 
+    for radius in range(1, 15):
 
-    max = img.shape[0]//2
-    min = img.shape[0]//10
-    radius = 4
-    for i in range(0, 1):
-        pos_x = int(random.random() * (max - min)) + int(min)
-        width_x = int(random.random() * (max - min)) + int(min)
-        pos_y = int(random.random() * (max - min)) + int(min)
-        height_y = int(random.random() * (40)) + 1
+        for i in range(0, 100):
+            max = img.shape[0] // 2
+            min = img.shape[0] // 10
+            radius = 4
 
-        print(pos_x)
-        print(pos_x + width_x)
-        print(pos_y)
-        print(pos_y + height_y)
+            pos_x = int(random.random() * (max - min)) + int(min)
+            width_x = int(random.random() * (max - min)) + int(min)
+            pos_y = int(random.random() * (max - min)) + int(min)
+            height_y = int(random.random() * (40)) + 1
 
-
-        mask = np.zeros(img.shape)
-        mask[pos_x: pos_x+width_x, pos_y: pos_y + height_y] = 255
-        mask = cv.linearPolar(mask, (mask.shape[1] // 2, mask.shape[0] // 2), mask.shape[1] // 2,
-                               cv.WARP_INVERSE_MAP + cv.WARP_FILL_OUTLIERS)
-        mask = np.uint8(mask)
-
-        print(cv.imwrite(outputMask, mask))
-        print(outputMask)
-
-        img[np.where(mask)] = 0
-        print(cv.imwrite("../../src/inpainting/img.png", img))
-
-        resNS = cv.inpaint(img, mask, radius, cv.INPAINT_NS)
-        resTelea = cv.inpaint(img, mask, radius, cv.INPAINT_TELEA)
+            print(pos_x)
+            print(pos_x + width_x)
+            print(pos_y)
+            print(pos_y + height_y)
 
 
-        print(cv.imwrite(outputPath1, resNS))
-        compare_images(resNS, img)
-        print(cv.imwrite(outputPath2, resTelea))
-        compare_images(resTelea, img)
-        print(outputPath1)
-        print(outputPath2)
+            mask = np.zeros(img.shape)
+            mask[pos_x: pos_x+width_x, pos_y: pos_y + height_y] = 255
+            mask = cv.linearPolar(mask, (mask.shape[1] // 2, mask.shape[0] // 2), mask.shape[1] // 2,
+                                   cv.WARP_INVERSE_MAP + cv.WARP_FILL_OUTLIERS)
+            mask = np.uint8(mask)
 
-        mask = None
-        img = None
-        resNS = None
-        resTelea = None
+            print(cv.imwrite(outputMask, mask))
+            print(outputMask)
+
+            img[np.where(mask)] = 0
+            print(cv.imwrite("../../src/inpainting/img.png", img))
+
+            resNS = cv.inpaint(img, mask, radius, cv.INPAINT_NS)
+            resTelea = cv.inpaint(img, mask, radius, cv.INPAINT_TELEA)
+
+
+            print(cv.imwrite(outputPath1, resNS))
+            compare_images(resNS, img)
+            print(cv.imwrite(outputPath2, resTelea))
+            compare_images(resTelea, img)
+            print(outputPath1)
+            print(outputPath2)
+
+            mask = None
+            img = None
+            resNS = None
+            resTelea = None
 
 
